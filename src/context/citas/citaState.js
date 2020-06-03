a357091e016f0ebfd9a82e420a3da231d1f4b4ba
Juan <juanjogonda@gmail.com>
2020-06-03 15:57:16 -0500

    Corrección citas

--- src/context/citas/citaState.js
+++ src/context/citas/citaState.js
@@ -69,7 +69,7 @@ const CitaState = props => {
             
             dispatch({
                 type: LISTAR_CITA,
-                payload: resultado.data.citas.filter(cita => cita.estado !== 'Cumplida')
+                payload: resultado.data.citas.filter(cita => cita.estado !== 'Cumplida' && cita.tipo === 'Tratamiento')
             })
         } catch (error) {
             console.log(error);                
