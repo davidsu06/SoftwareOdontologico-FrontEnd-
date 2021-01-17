d0c5ce2c839fa7621c78788d1f4139587e3ce9fd
Gondax27 <juanjogonda@gmail.com>
2021-01-17 16:07:52 -0500

    Corrección link servidor

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-    // baseURL : 'https://stark-dusk-34785.herokuapp.com'
-    baseURL : 'http://localhost:4000'
+    baseURL : 'https://stark-dusk-34785.herokuapp.com'
+    // baseURL : 'http://localhost:4000'
 });
 
 export default clienteAxios;
